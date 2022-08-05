19c339c9ca4a6eca631b85252d0dfa17425fc4df
klozz <carlosj@klozz.dev>
2022-08-05 08:06:44 +0200

    sm8250-common: Switch to GrapheneOS Camera
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor
 
 PRODUCT_PACKAGES += \
-    Camera2
+    Camera
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
